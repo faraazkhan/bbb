5343e6aa4ef9dd784cac3533b702cc2d18803d36
Faraaz Khan <ruby@rationalizeit.us>
2011-11-04 03:11:46 -0400

    Better Business Analysis!

---
+++ Ajax.php
@@ -0,0 +1,2 @@
+ iffathkhan06@yahoo.co.in at 183.83.244.196
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbb::Application.config.secret_token = 'c8fe7aaf5887808a2df73ce9da1c8ed1f7ae9e22bc19b92179f73b31fb9315c812fb4d36a818a04405bdd81531fad1770c3a9ab85b25abaee2ed1446d8918ef6'
